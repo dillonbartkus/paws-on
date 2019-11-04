06420696f3ffb3d12e0e616a9ff42ae1f090e2fc
unknown <dillonbartkus@gmail.com>
2019-11-04 17:59:35 -0500

    'initial'

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = process.env.HTTP_PORT || 3001;
+const cors = require('cors')
+const parser = require('body-parser');
+
+app.use(cors())
+app.use(parser.json());
+app.use(parser.urlencoded({extended: false}));
+
+app.get('/', (req, res) => {
+  res.send(`server`)
+})
+
+// const routes = require('./routes/routes')
+// app.use('/', routes)
+
+app.listen(port, () => {  
+  console.log(`Listening on ${port}`)
+})
---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+
+export default function App() {
+
+  return (
+
+    <div className="App">
+
+      <p>asdasdasd</p>
+
+    </div>
+  )
+}
